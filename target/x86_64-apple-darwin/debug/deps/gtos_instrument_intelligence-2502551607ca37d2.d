7c27aa7b62611340ffa711e55a07c9b277cc7500
tysonkenobi <tysontaber@gmail.com>
2026-06-27 11:15:28 -0600

    10.6.1 cont
    reassessment of hw telemetry addition and new layer 4/5/6 defintion including their tier 2 testing requirements

---
+++ target/x86_64-apple-darwin/debug/deps/gtos_instrument_intelligence-2502551607ca37d2.d
@@ -0,0 +1,6 @@
+/Users/tnt/Desktop/Project-GTOS/target/x86_64-apple-darwin/debug/deps/gtos_instrument_intelligence-2502551607ca37d2.d: apps/gtos_ai_bridge_universal.rs apps/gtos_modulator_core.rs
+
+/Users/tnt/Desktop/Project-GTOS/target/x86_64-apple-darwin/debug/deps/gtos_instrument_intelligence-2502551607ca37d2: apps/gtos_ai_bridge_universal.rs apps/gtos_modulator_core.rs
+
+apps/gtos_ai_bridge_universal.rs:
+apps/gtos_modulator_core.rs:
---
+++ target/x86_64-apple-darwin/debug/deps/gtos_instrument_motherboard-90876ed0ec95a09f.d
@@ -0,0 +1,6 @@
+/Users/tnt/Desktop/Project-GTOS/target/x86_64-apple-darwin/debug/deps/gtos_instrument_motherboard-90876ed0ec95a09f.d: apps/gtos_motherboard_core.rs apps/gtos_modulator_core.rs
+
+/Users/tnt/Desktop/Project-GTOS/target/x86_64-apple-darwin/debug/deps/gtos_instrument_motherboard-90876ed0ec95a09f: apps/gtos_motherboard_core.rs apps/gtos_modulator_core.rs
+
+apps/gtos_motherboard_core.rs:
+apps/gtos_modulator_core.rs:
